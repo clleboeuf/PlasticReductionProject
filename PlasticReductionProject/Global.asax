430fbbbaf6afaf8a373d9c3c676045a0c5c95497
AndrewBrown624 <94208615+AndrewBrown624@users.noreply.github.com>
2023-01-16 18:04:48 +1100

    Add project files.

---
+++ PlasticReductionProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlasticReductionProject.MvcApplication" Language="C#" %>
---
+++ PlasticReductionProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlasticReductionProject.Startup))]
+namespace PlasticReductionProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

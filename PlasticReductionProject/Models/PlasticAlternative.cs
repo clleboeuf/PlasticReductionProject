22f8be8633aaf853329e99e5c053995f2ac4605b
terry madden <tmad0013@student.monash.edu>
2023-01-29 09:10:43 +1000

    database csv seed WIP

--- PlasticReductionProject/Models/PlasticAlternative.cs
+++ PlasticReductionProject/Models/PlasticAlternative.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PlasticReductionProject.Models
+{
+    public class PlasticAlternative
+    {
+        [Key]
+        [Name("WordDictionaryId")]
+        public int Id { get; set; }
+
+        [Name("WordText")]
+        public string Word { get; set; }
+
+        [Name("WordDefinition")]
+        public string Definition { get; set; }
+
+    }
+}

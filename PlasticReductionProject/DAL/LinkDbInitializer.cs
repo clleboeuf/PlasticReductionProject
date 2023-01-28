3abdf61f623a6c328794d48ad5572b5a613db91b
AndrewBrown624 <94208615+AndrewBrown624@users.noreply.github.com>
2023-01-28 14:37:51 +1100

    links should be working

--- PlasticReductionProject/DAL/LinkDbInitializer.cs
+++ PlasticReductionProject/DAL/LinkDbInitializer.cs
@@ -5,14 +5,14 @@ using System.Data.Entity.Migrations;
 
 namespace PlasticReductionProject.DAL
 {
-    internal class LinkDbInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<LinkDbContext>  //  DropCreateDatabaseIfModelChanges<ItemDbContext>  or DropCreateDatabaseAlways<ItemDbContext>
+    internal class LinkDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<LinkDbContext>  //  DropCreateDatabaseIfModelChanges<ItemDbContext>  or DropCreateDatabaseAlways<ItemDbContext>
     {
         protected override void Seed(LinkDbContext context)
         {
             var links = new List<Link>
             {
-            new Link{Description="Gardening Equipment and Tools"}
-
+            new Link{Description="Gardening Equipment and Tools",Url="test",Image="blah"},
+            new Link{Description="Another Record",Url="test",Image="blah"}
             };
             links.ForEach(i => context.Links.AddOrUpdate(i));
             context.SaveChanges();

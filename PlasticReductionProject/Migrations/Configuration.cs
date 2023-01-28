be80dd11fbe0ba4442b5982b139e5012023dd2d1
terry madden <tmad0013@student.monash.edu>
2023-01-28 18:49:29 +1000

    Projects database seeded

---
+++ PlasticReductionProject/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PlasticReductionProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PlasticReductionProject.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PlasticReductionProject.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

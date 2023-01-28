23976f1292555b7ad3709dbff828bc0688fdff43
AndrewBrown624 <94208615+AndrewBrown624@users.noreply.github.com>
2023-01-29 10:47:43 +1100

    redone stuff

--- PlasticReductionProject/DAL/LinkDbInitializer.cs
+++ PlasticReductionProject/DAL/LinkDbInitializer.cs
@@ -21,37 +21,6 @@ namespace PlasticReductionProject.DAL
             context.SaveChanges();
 
 
-            var projects = new List<Project>
-            {
-            new Project{
-                Name="Plastic Free Party",
-                Description="Help protect the planet while having a good time!  Why not make your next party plastic free? ",
-                LinkUrl="https://www.natgeokids.com/au/kids-club/cool-kids/general-kids-club/throw-a-plastic-free-party/",
-                Image="../Images/ProjectImages/plastic_Free_party_1.jpeg"},
-            new Project{
-                Name="Rubbish Audit",
-                Description="Do you know what you are throwing away? And where is this place \"away\"? Take a closer look and see if you can make greener choices ",
-                LinkUrl="https://sustainableinthesuburbs.com/2021/04/23/how-to-conduct-a-trash-audit-with-kids/",
-                Image="../Images/ProjectImages/Audit_IMG_4785-1030x772.jpeg"},
-            new Project{
-                Name="Build a Food Garden",
-                Description="Growing your own food is even more satisfying when you recognise the plastic packaging avoided",
-                LinkUrl="https://www.bunnings.com.au/diy-advice/garden/planting-and-growing/gardening-for-kids",
-                Image="../Images/ProjectImages/grow-where-you-are-community-food-garden.jpg"},
-            new Project{
-                Name="Make plastic free food wrap",
-                Description="Make you own environmentally friendly, reusable food wrap",
-                LinkUrl="https://www.theplasticfreeshop.com.au/2019/06/06/diy-beeswax-foodwraps/\r\n",
-                Image="../Images/ProjectImages/Foodwraps_med_strawberries-scaled-721x487.jpg"},
-            new Project{
-                Name="Save a Beach",
-                Description="\"Small acts add up\".  Every piece of plastic you remove from a beach is one more prevented from entering our oceans ",
-                LinkUrl="https://www.4ocean.com/blogs/blog/how-to-organize-a-community-beach-cleanup",
-                Image="../Images/ProjectImages/istockphoto-1023573774-612x612.jpg"},
-
-            };
-            projects.ForEach(i => context.Projects.AddOrUpdate(i));
-            context.SaveChanges();
         }
     }
 }

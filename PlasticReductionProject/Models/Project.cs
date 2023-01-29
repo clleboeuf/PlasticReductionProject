19d72526daae3b0ceb0e0736614614207de60ba2
terry madden <tmad0013@student.monash.edu>
2023-01-29 15:12:22 +1000

    Projects data seeded from csv

--- PlasticReductionProject/Models/Project.cs
+++ PlasticReductionProject/Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,29 +10,16 @@ namespace PlasticReductionProject.Models
     public class Project
     {
         [Key]
+        [Name("Project_Id")]
         public int Id { get; set; }
-
+        [Name("ProjectName")]
         public string Name { get; set; }
-
+        [Name("ProjectDescription")]
         public string Description { get; set; }
         // server path to image as a string so it can be stored in database
+        [Name("ProjectLink")]
         public string LinkUrl { get; set; }
-
+        [Name("ProjectImage")]
         public string Image { get; set; }
-
-
-
-        //public virtual Uri Url
-        //{
-        //    get
-        //    {
-        //        if (this.LinkUrl != null)
-        //        {
-        //            return new Uri(this.LinkUrl);
-        //        }
-        //        return null;
-        //    }
-
-        //}
     }
 }

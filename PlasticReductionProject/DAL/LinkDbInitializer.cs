6ba600d959d3b38008bfb0da261d5ffe236c7e6c
terry madden <tmad0013@student.monash.edu>
2023-01-29 14:48:05 +1000

    Links seed converted to csv

--- PlasticReductionProject/DAL/LinkDbInitializer.cs
+++ PlasticReductionProject/DAL/LinkDbInitializer.cs
@@ -16,15 +16,22 @@ namespace PlasticReductionProject.DAL
     {
         protected override void Seed(LinkDbContext context)
         {
-            var links = new List<Link>
-            {
-            new Link{Description="Click the logo to go to the World Wildlife Fund's page on plastic pollution in Australia which provides a lot of information on the effect that plastic has on the animals in the natural world.",Url="https://www.wwf.org.au/get-involved/plastic-pollution/stopping-plastic-pollution",Image="../Images/LinksImages/WWFLogo.jpg"},
-            new Link{Description="The Plastic Soup Foundation provides information  on the effect that plastic has on the oceans and also has a lot of information about how plastic is getting into our food sources.  If you click the logo you will be taken to their website where you can find a lot of information to help you make a difference.",Url="https://www.plasticsoupfoundation.org/en/",Image="../Images/LinksImages/PSFlogo.jpg" },
-            new Link{Description="The United Nations Environment Programme provides global information about plastic pollution and will tell you about efforts around the world to stop the pollution of our environment by plastic.  Just click the log to find out more.",Url="https://www.unep.org/plastic-pollution",Image="../Images/LinksImages/UNEPLogo.png"},
-            new Link{Description="As one of the country’s most recognised and trusted environmental organisations, Clean Up Australia has helped Australians take practical environmental action for over 30 years.  In fact Clean Up Australia Day is now the nation's largest community-based environmental event. Find out how Australia has a plastics problem. Australia now produces 2.5 million tonnes of plastic waste each year, equating to 100 kg per person. Of this, only 13% of plastic is recovered and 84% is sent to landfill.",Url="https://www.cleanup.org.au/our-waste-challenges",Image="../Images/LinksImages/CleanUpLogo.jpg"}
-            };
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"./DAL/links.csv");
+            var reader = new StreamReader(path);
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var links = csv.GetRecords<Link>();
             links.ForEach(i => context.Links.AddOrUpdate(i));
             context.SaveChanges();
+            //var links = new List<Link>
+            //{
+            //new Link{Description="Click the logo to go to the World Wildlife Fund's page on plastic pollution in Australia which provides a lot of information on the effect that plastic has on the animals in the natural world.",Url="https://www.wwf.org.au/get-involved/plastic-pollution/stopping-plastic-pollution",Image="../Images/LinksImages/WWFLogo.jpg"},
+            //new Link{Description="The Plastic Soup Foundation provides information  on the effect that plastic has on the oceans and also has a lot of information about how plastic is getting into our food sources.  If you click the logo you will be taken to their website where you can find a lot of information to help you make a difference.",Url="https://www.plasticsoupfoundation.org/en/",Image="../Images/LinksImages/PSFlogo.jpg" },
+            //new Link{Description="The United Nations Environment Programme provides global information about plastic pollution and will tell you about efforts around the world to stop the pollution of our environment by plastic.  Just click the log to find out more.",Url="https://www.unep.org/plastic-pollution",Image="../Images/LinksImages/UNEPLogo.png"},
+            //new Link{Description="As one of the country’s most recognised and trusted environmental organisations, Clean Up Australia has helped Australians take practical environmental action for over 30 years.  In fact Clean Up Australia Day is now the nation's largest community-based environmental event. Find out how Australia has a plastics problem. Australia now produces 2.5 million tonnes of plastic waste each year, equating to 100 kg per person. Of this, only 13% of plastic is recovered and 84% is sent to landfill.",Url="https://www.cleanup.org.au/our-waste-challenges",Image="../Images/LinksImages/CleanUpLogo.jpg"}
+            //};
+            //links.ForEach(i => context.Links.AddOrUpdate(i));
+            //context.SaveChanges();
 
             var projects = new List<Project>
             {
@@ -59,9 +66,9 @@ namespace PlasticReductionProject.DAL
             context.SaveChanges();
 
 
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"./DAL/Dictionary.csv");
-            var reader = new StreamReader(path);
-            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"./DAL/Dictionary.csv");
+            reader = new StreamReader(path);
+            csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             var dictionary = csv.GetRecords<DictionaryWord>();
             dictionary.ForEach(i => context.Dictionary.AddOrUpdate(i));
             context.SaveChanges();

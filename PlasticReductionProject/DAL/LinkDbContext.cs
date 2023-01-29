d98d4c8bb1cea8c9248a5ef5a7c03be2460f5a32
terry madden <tmad0013@student.monash.edu>
2023-01-29 13:41:05 +1000

    Database seed WIP update

--- PlasticReductionProject/DAL/LinkDbContext.cs
+++ PlasticReductionProject/DAL/LinkDbContext.cs
@@ -18,6 +18,7 @@ namespace PlasticReductionProject.DAL
         public DbSet<Link> Links { get; set; }
         public DbSet<Project> Projects { get; set; }    
         public DbSet<DictionaryWord> Dictionary { get; set; }
+
     //    public DbSet<Product> Products { get; set; }
         public DbSet<PlasticAlternative> Alternatives { get; set; }
 

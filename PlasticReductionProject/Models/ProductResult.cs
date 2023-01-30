173e7a060f598d0f4814c409940f2b25d8c374b3
Christopher Le Boeuf <cleb0002@student.monash.edu>
2023-01-30 22:44:42 +1100

    Adding the models for Product and Calculator results.

--- PlasticReductionProject/Models/ProductResult.cs
+++ PlasticReductionProject/Models/ProductResult.cs
@@ -5,8 +5,17 @@ using System.Web;
 
 namespace PlasticReductionProject.Models
 {
-    public class CalculatorResult
-    {
+    public enum FrequencySelection { Never, Sometimes, Often, Always }
 
+    public enum TimePeriodSelection { Day, Week, Month, Year }
+
+    public class ProductResults
+    {
+        public int Id { get; set; }
+
+        public int AmountUsed { get; set; }
+
+        public int AmountRecycled { get; set; }
     }
+
 }

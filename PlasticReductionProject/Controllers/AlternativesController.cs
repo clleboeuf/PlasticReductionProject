1924b4997a0dc57f030ec50f84b54472052ce38b
AndrewBrown624 <94208615+AndrewBrown624@users.noreply.github.com>
2023-01-30 21:41:50 +1100

    Alternatives and alternative details page working better

--- PlasticReductionProject/Controllers/AlternativesController.cs
+++ PlasticReductionProject/Controllers/AlternativesController.cs
@@ -23,20 +23,8 @@ namespace PlasticReductionProject.Controllers
                                                         on productID.Id equals AltProduct.ProductId
                                                         select productID)
                                                         .ToList().Distinct().ToList();
-
             return View(ProductList);
         }
-
-   //     public ActionResult AlternativesDetails()
-   //     {
-   //         ViewBag.Page = "Alternatives";
-
-   //         var productName = db.Alternatives.ToList().FirstOrDefault();
-
-   //         ViewBag.Product = productName.ProductId;
-
-   //         return View(db.Alternatives.ToList());
-   //     }
 
         public ActionResult AlternativesDetails(int ProductID)
         {
@@ -49,10 +37,6 @@ namespace PlasticReductionProject.Controllers
                                .ToList().Distinct().ToList().FirstOrDefault().Description.ToString();
 
             var productName = db.Alternatives.Where(test => test.ProductId == ProductID).ToList();
-
-         //   ViewBag.Product = productName.  .ProductId;
-
-          //  return View(db.Alternatives.ToList());
 
             return View(productName);
         }
